e987e0a27f24ce54cceee763028ad49b178d61da
Nguyen Toai <nguyentoai.dev@gmail.com>
2020-03-09 06:02:43 +0700

    [FIX] Fix douplicate code

--- static/src/js/message.js
+++ static/src/js/message.js
@@ -2,7 +2,7 @@ odoo.define("pos_message.message", function (require) {
     "use strict";
     let rpc = require('web.rpc');
     let chrome = require('point_of_sale.chrome');
-    // let models = require('point_of_sale.models');
+    let models = require('point_of_sale.models');
     let screens = require('point_of_sale.screens');
 
     // Model check data
@@ -29,56 +29,7 @@ odoo.define("pos_message.message", function (require) {
     //     //
     //     // }
     // });
-    screens.ReceiptScreenWidget.include({
-        // When user click Next order in receipt screen
-        get_message_receipt: async function () {
-            // let domain = [['id', '=', message_id]];
-            let self = this;
-            let time_now = moment().format('YYYY-MM-DDTHH:mm:ss');
-            let domain = [['pos_message_line_ids.pos_config_id', '=', self.pos.config.id],
-                ['message_time', '<', time_now],
-                ['pos_message_line_ids.is_read', '=', false]];
-            let fields = ['name', 'message'];
-            return await rpc.query({
-                model: 'pos.message',
-                method: 'search_read',
-                args: [domain, fields],
-                limit: 1,
-            });
-        },
-        confirm_message_receipt: async function (message_id, pos_config_id) {
-            return await rpc.query({
-                model: 'pos.message.line',
-                method: 'create_from_ui',
-                args: [{'is_read': true, 'message_id': message_id, 'pos_config_id': pos_config_id}],
-            });
-        },
-        click_next: async function () {
-            let self = this;
-            let message = await self.get_message_receipt();
-            if (message.length > 0) {
-                self.gui.show_popup('confirm', {
-                    'title': message[0].name,
-                    'body': message[0].message,
-                    'confirm': async function () {
-                        let ok = await self.confirm_message_receipt(message[0].id, self.pos.config.id);
-                        if (ok) {
-                            window.location.reload();
-                        } else {
-                            self.pos.get_order().finalize();
-                        }
-                    },
-                    'cancel': function () {
-                        self.pos.get_order().finalize();
-                    }
-                });
-            } else {
-                self.pos.get_order().finalize();
-            }
-        }
-    });
-    //HOOK chrome
-    chrome.OrderSelectorWidget.include({
+    models.PosModel = models.PosModel.extend({
         get_message: async function () {
             // let domain = [['id', '=', message_id]];
             let self = this;
@@ -101,6 +52,35 @@ odoo.define("pos_message.message", function (require) {
                 args: [{'is_read': true, 'message_id': message_id, 'pos_config_id': pos_config_id}],
             });
         },
+    });
+    screens.ReceiptScreenWidget.include({
+        // When user click Next order in receipt screen
+        click_next: async function () {
+            let self = this;
+            let message = await self.pos.get_message();
+            if (message.length > 0) {
+                self.gui.show_popup('confirm', {
+                    'title': message[0].name,
+                    'body': message[0].message,
+                    'confirm': async function () {
+                        let ok = await self.pos.confirm_message(message[0].id, self.pos.config.id);
+                        if (ok) {
+                            window.location.reload();
+                        } else {
+                            self.pos.get_order().finalize();
+                        }
+                    },
+                    'cancel': function () {
+                        self.pos.get_order().finalize();
+                    }
+                });
+            } else {
+                self.pos.get_order().finalize();
+            }
+        }
+    });
+    //HOOK chrome
+    chrome.OrderSelectorWidget.include({
         show_message_handler: async function (is_neworder = false) {
             let self = this;
             let message = await self.get_message();
